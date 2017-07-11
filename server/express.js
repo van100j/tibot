56bfb0516cec594b97203f06caa64d2f929c978a
Vanco Stojkov <van100j@gmail.com>
2017-07-12 01:28:49 +0200

    static files

--- server/express.js
+++ server/express.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
+//
+// router.use('/*', function(req, res){
+//   return res.sendFile('index.html', {
+//     root: 'client/dist/'
+//   });
+// });
+// app.use(router);
 
-router.use('/*', function(req, res){
-  return res.sendFile('index.html', {
-    root: 'client/dist/'
-  });
-});
-app.use(router);
+app.use(express.static('client/dist'))
 
 module.exports = app;
