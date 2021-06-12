7d9a5bff761a0571f3fc17fc30b4b16d4b4286c8
Vancho Stojkov <van100j@gmail.com>
2021-06-12 03:59:13 +0200

    Switch api.ai to dialogflow (#25)
    * Switch api.ai to dialogflow
    * Cleanup

--- README.md
+++ README.md
@@ -9,17 +9,17 @@ A simple bot app which enables users to ask about the current date and time, cal
 
 ## Requirements
 
-The app is built using [Angular](https://angular.io/) on the front end, [Express](https://expressjs.com/) on the back end, and [API.AI](https://api.ai/)'s API to process and understand natural language.
+The app is built using [Angular](https://angular.io/) on the front end, [Express](https://expressjs.com/) on the back end, and ~~[API.AI](https://api.ai/)'s API~~ [Dialogflow](https://cloud.google.com/dialogflow/) to process and understand natural language.
 
 You will need:
 - [Node.js](https://nodejs.org/en/), and [npm](https://docs.npmjs.com/getting-started/installing-node) or [Yarn](https://yarnpkg.com/en/)
-- [API.AI account](https://console.api.ai/api-client/#/login)
+- ~~[API.AI account](https://console.api.ai/api-client/#/login)~~ [Google Cloud account](https://console.cloud.google.com/), and [setup it up to start using Dialogflow](https://cloud.google.com/dialogflow/es/docs/quick/setup)
 
 You will also need [Google Maps API](https://developers.google.com/maps/get-started/) key, as we'll use some of their APIs to get data related to locations and timezones, specifically:
 - The [Google Maps Geocoding API](https://developers.google.com/maps/documentation/geocoding/start) — to get location related data
 - The [Google Time Zone API](https://developers.google.com/maps/documentation/timezone/intro) — to get location's timezone data.
 
-Once you've created your [API.AI account](https://console.api.ai/api-client/#/login) go ahead and create your [Agent](https://api.ai/docs/agents). Then, in your created Agent, import the [intents](./data/intents) and [entities](./data/entities) used in this example.
+Once you've created your ~~[API.AI account](https://console.api.ai/api-client/#/login)~~ [Google Cloud account](https://console.cloud.google.com/) go ahead and create your [Agent](https://cloud.google.com/dialogflow/es/docs/quick/build-agent). Then, in your created Agent, import the [intents](./data/intents) and [entities](./data/entities) used in this example.
 
 ## Getting Started
 
--- server/intents/index.js
+++ server/intents/index.js
@@ -1,63 +1,107 @@
-const apiai = require("apiai")(process.env.APIAI_CLIENT_TOKEN);
-const { v4: uuidv4 } = require('uuid');
+const dialogflow = require("@google-cloud/dialogflow");
+const { v4: uuidv4 } = require("uuid");
+const { value, struct } = require("pb-util");
 
 // include and merge all intents:
-const date = require('./date');
-const time = require('./time');
+const date = require("./date");
+const time = require("./time");
 const intents = Object.assign({}, date, time);
 
-// Process intents at API.AI, and get action and parameters
-const callApiAi = (text, sessionId, tz) => new Promise((resolve, reject) => {
-  const request = apiai.textRequest(text, { sessionId: sessionId, timezone: tz });
+// GOOGLE Cloud credentials and Dialogflow project id from .env
+const {
+  GOOGLE_APPLICATION_CREDENTIALS_PKEY,
+  GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL,
+  DIALOGFLOW_PROJECT_ID,
+} = process.env;
 
-  request.on('response', response => resolve(response));
-  request.on('error', error => reject(error));
-  request.end();
+const dialogflowSessionClient = new dialogflow.SessionsClient({
+  credentials: {
+    private_key: GOOGLE_APPLICATION_CREDENTIALS_PKEY.replace(/\\n/g, '\n'),
+    client_email: GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL,
+  },
 });
+
+async function dialogflowRequest(text, sessionId) {
+  const sessionPath = dialogflowSessionClient.projectAgentSessionPath(
+    DIALOGFLOW_PROJECT_ID,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text,
+        languageCode: "en-US",
+      },
+    },
+  };
+
+  const [{ queryResult: result }] = await dialogflowSessionClient.detectIntent(
+    request
+  );
+
+  return result;
+}
+
+const parseParameters = ({ fields }) => {
+  return Object.keys(fields).reduce((acc, key) => {
+    const { kind, structValue } = fields[key];
+
+    acc[key] =
+      kind === "structValue"
+        ? struct.decode(structValue)
+        : value.decode(fields[key]);
+
+    return acc;
+  }, {});
+};
 
 // Process the action
 const doIntent = (response, tz) => {
-  const { parameters, action, fulfillment } = response.result;
+  const { parameters: pars, action, fulfillmentText: fulfillment } = response;
+  const parameters = parseParameters(pars);
 
   return new Promise((resolve, reject) => {
     if (intents[action]) {
       return resolve(intents[action](parameters, tz));
-    } else if (fulfillment.speech) {
-      return resolve(fulfillment.speech);
+    } else if (fulfillment) {
+      return resolve(fulfillment);
     }
     return reject(handleUnknownAnswer());
   });
-}
+};
 
 const handleUnknownAnswer = (err) => {
+  console.error(err);
   const msgs = [
-    'Didn\'t quite catch what you said?',
-    'Donno',
-    'Can you try again?',
-    'Sorry, can\'t help you with that?'
+    "Didn't quite catch what you said?",
+    "Donno",
+    "Can you try again?",
+    "Sorry, can't help you with that?",
   ];
 
   return msgs[~~(Math.random() * msgs.length)];
 };
 
-const processRequest = (msg) => new Promise((resolve, reject) => {
-  // try/catch to make sure we don't crush on invalid JSON msgs
-  try {
-    const input = JSON.parse(msg);
+const processRequest = (payload) =>
+  new Promise((resolve, reject) => {
+    // try/catch to make sure we don't crush on invalid JSON msgs
+    try {
+      const input = JSON.parse(payload);
+      const { type, tz, msg, sessionId = uuidv4() } = input
 
-    // process our users' request only
-    if (input.type === 'user' && input.msg) {
-      const sessionId = input.sessionId || uuidv4();
-      const tz = input.tz;
-
-      callApiAi(input.msg, sessionId, tz)
-        .then(response => doIntent(response, tz))
-        .then(answer => resolve(answer))
-        .catch(err => resolve(handleUnknownAnswer(err)))
+      // process our users' request only
+      if (type === "user" && msg) {
+        dialogflowRequest(msg, sessionId)
+          .then((response) => doIntent(response, tz))
+          .then((answer) => resolve(answer))
+          .catch((err) => resolve(handleUnknownAnswer(err)));
+      }
+    } catch (err) {
+      resolve(handleUnknownAnswer(err));
     }
-  } catch (err) {
-    resolve(handleUnknownAnswer(err));
-  }
-});
+  });
 
 module.exports = processRequest;
